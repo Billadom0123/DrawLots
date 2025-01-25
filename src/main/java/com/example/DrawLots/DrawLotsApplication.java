8655cc52ce08d72f35491348499e042f70580602
Billadom0123 <3233405848@qq.com>
2025-01-25 19:13:52 +0800

    first commit

---
+++ src/main/java/com/example/DrawLots/DrawLotsApplication.java
@@ -0,0 +1,13 @@
+package com.example.DrawLots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrawLotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrawLotsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DrawLots/DrawLotsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DrawLots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrawLotsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
