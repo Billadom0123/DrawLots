d0b7050bc9fb085b36aaa67c0d4e33fea2cb9064
Supreme-Demon-Dragon <3020902816@qq.com>
2025-02-07 18:40:16 +0800

    这里面的参数与接口文档对应

--- src/main/java/com/example/DrawLots/controller/DrawLotsController.java
+++ src/main/java/com/example/DrawLots/controller/DrawLotsController.java
@@ -16,16 +16,16 @@ public class DrawLotsController {
     private DrawLotsService drawLotsService;
 
     @GetMapping("/lots/info")
-    public Response getLotsInfo(@RequestParam("sid") Integer sid)
+    public Response getLotsInfo(@RequestParam("id") Integer id)
     {
-        return drawLotsService.getLotsInfo(sid);
+        return drawLotsService.getLotsInfo(id);
     }
 
     @PostMapping("/lots/join")
-    public Response joinLots(@RequestParam("sid") Integer sid,
+    public Response joinLots(@RequestParam("id") Integer id,
                              @RequestParam("uid") Integer uid)
     {
-        return drawLotsService.joinLots(sid,uid);
+        return drawLotsService.joinLots(id,uid);
     }
 
 }
